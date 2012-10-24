8fb38f148db9111913848fd0b700e95627d47773
Clay Schubiner <cschubiner@gmail.com>
2012-10-24 14:30:51 -0700

    commit

--- examples/4 - Databases/tictactoe/clear.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("config.php");
-
-$result = mysql_query("delete from tictactoe where player = '".$_POST["player"]."' and against = '".$_POST["against"]."'");
-$result = mysql_query("delete from tictactoe where player = '".$_POST["against"]."' and against = '".$_POST["player"]."'");
-
-?>
--- examples/4 - Databases/tictactoe/savemove.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("config.php");
-
-$result = mysql_query("insert into tictactoe values ('".$_POST["game"]."','".$_POST["player"]."','".$_POST["against"]."','".$_POST["move"]."','".$_POST["ch"]."');");
-$result = mysql_query("insert into tictactoe values ('".$_POST["game"]."','".$_POST["against"]."','".$_POST["player"]."','".$_POST["move"]."','".$_POST["ch"]."');");
-?>
